05931be4eb88b7cb02c6ef836ce47acc1bb2649a
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-09-18 09:32:46 +0200

    Add files via upload

---
+++ UpisivanjePodataka_ponavljanje/UpisivanjePodataka_ponavljanje/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace UpisivanjePodataka_ponavljanje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtGodinaRodjenja = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnUpis = new System.Windows.Forms.Button();
+            this.btnBrisanje = new System.Windows.Forms.Button();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(194, 54);
+            this.txtIme.Multiline = true;
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(223, 20);
+            this.txtIme.TabIndex = 0;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(194, 100);
+            this.txtPrezime.Multiline = true;
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(223, 26);
+            this.txtPrezime.TabIndex = 1;
+            // 
+            // txtGodinaRodjenja
+            // 
+            this.txtGodinaRodjenja.Location = new System.Drawing.Point(194, 157);
+            this.txtGodinaRodjenja.Multiline = true;
+            this.txtGodinaRodjenja.Name = "txtGodinaRodjenja";
+            this.txtGodinaRodjenja.Size = new System.Drawing.Size(235, 26);
+            this.txtGodinaRodjenja.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(194, 210);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(235, 26);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Prezime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "GodinaRodjenja";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email";
+            // 
+            // btnUpis
+            // 
+            this.btnUpis.Location = new System.Drawing.Point(40, 299);
+            this.btnUpis.Name = "btnUpis";
+            this.btnUpis.Size = new System.Drawing.Size(88, 31);
+            this.btnUpis.TabIndex = 8;
+            this.btnUpis.Text = "Upis";
+            this.btnUpis.UseVisualStyleBackColor = true;
+            this.btnUpis.Click += new System.EventHandler(this.btnUpis_Click);
+            // 
+            // btnBrisanje
+            // 
+            this.btnBrisanje.Location = new System.Drawing.Point(219, 299);
+            this.btnBrisanje.Name = "btnBrisanje";
+            this.btnBrisanje.Size = new System.Drawing.Size(84, 31);
+            this.btnBrisanje.TabIndex = 9;
+            this.btnBrisanje.Text = "Brisanje";
+            this.btnBrisanje.UseVisualStyleBackColor = true;
+            this.btnBrisanje.Click += new System.EventHandler(this.btnBrisanje_Click);
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(374, 299);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(84, 31);
+            this.btnSpremi.TabIndex = 10;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.btnBrisanje);
+            this.Controls.Add(this.btnUpis);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtGodinaRodjenja);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtGodinaRodjenja;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnUpis;
+        private System.Windows.Forms.Button btnBrisanje;
+        private System.Windows.Forms.Button btnSpremi;
+    }
+}
+
